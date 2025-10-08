7859c386c525c82a29d6a525a537f95a75a98537
Aetshi <ouvrebaptiste@gmail.com>
2025-10-08 16:24:58 +0200

    Update Brouillon1.py

--- Code/Brouillon1.py
+++ Code/Brouillon1.py
@@ -1,1 +1,6 @@
-print("hello word ! :) ")
+#a mettre dans la classe Graphe
+
+def arete_case(self, case):
+  lettre = 'abcdefghijkl'
+  except case.nom[0] not in lettre or (int(case.nom[1:])>12 and int(case.nom[1:])<1)
+  
