a4e332a283b5f8440e32a3a3434133f4badc3a35
Aetshi <ouvrebaptiste@gmail.com>
2026-02-25 14:51:51 +0100

    Modification de la page d'accueil

--- Code/static/scripts/index.js
+++ Code/static/scripts/index.js
@@ -58,3 +58,11 @@ function goToTutoriel(mode) {
     })
     .catch(err => console.error("Erreur lors de la création de la partie :", err));
 }
+
+function openNav() {
+    document.getElementById("nav_left").style.display = "flex";
+}
+
+function closeNav() {
+    document.getElementById("nav_left").style.display = "none";
+}
