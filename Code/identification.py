8279e7e4a62edf4a55d87d47c8d31e7ab2579e40
Aetshi <ouvrebaptiste@gmail.com>
2026-03-05 10:40:56 +0100

    Ajout du code d'un système d'identification

---
+++ Code/identification.py
@@ -0,0 +1,51 @@
+
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+import uuid
+import sqlite3
+import os
+
+cwd = os.getcwd()
+code_dir = os.path.join(cwd, "Code")
+os.chdir(code_dir)
+ph = PasswordHasher()
+
+def register(username,password):
+    password_hash = ph.hash(password)
+    player_id = str(uuid.uuid4())
+    conn = sqlite3.connect("user_database.sqlite")
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""INSERT INTO user_table (user_uuid, user_id, user_password) VALUES (?, ?, ?)""", (player_id, username, password_hash))
+        conn.commit()
+        print(f"Utilisateur {username} créé avec succès")
+        return player_id
+    except sqlite3.IntegrityError:
+        print("Erreur : cet identifiant existe déjà")
+        return None
+    finally:
+        conn.close()
+
+def login(username,password):
+    conn = sqlite3.connect("user_database.sqlite")
+    cursor = conn.cursor()
+    stored_user = cursor.execute("""SELECT user_uuid, user_password FROM user_table WHERE user_id = ?""", (username,)).fetchone()
+    conn.close()
+    if stored_user:
+        stored_password = stored_user[1]
+    else:
+        print("Erreur : identifiant incorrect")
+        return None
+    try:
+        ph.verify(stored_password,password)
+        print(f"Utilsateur {username} connecté avec succès")
+        return stored_user[0] 
+    except VerifyMismatchError:
+        print("Erreur : mot de passe incorrect")
+        return None
+
+if __name__ == "__main__":
+    register("Odeline","BestPersonEver")
+    login("Odeline","BestPersonEver")
+    login("Odelin","BestPersonEver")
+    login("Odeline","WorstPersonEver")
